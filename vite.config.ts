f43eb0bc65190540da162abd03a96892074e5105
tane-git <tane.alexander.wilson@gmail.com>
2022-02-26 10:12:35 +1300

    add less to vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          '@btn-primary-bg': '#4388DD',
+          '@component-background': '#4388DD'
+        }
+      }
+    }
+  },
   plugins: [react()]
 })

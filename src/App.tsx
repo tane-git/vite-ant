e9a4836b56ff3c78a78673f6d3cf78ac25b25fd1
tane-git <tane.alexander.wilson@gmail.com>
2022-02-26 10:19:17 +1300

    add antd layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,22 @@ import { Nav } from './components';
 // antd
 // import 'antd/dist/antd.css';
 import 'antd/dist/antd.less';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
-    <div className="App">
-      <div>
+    <Layout>
+      <Header>
         <Nav />
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p><PrimaryButton /></p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is very hot: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      </Header>
+      <Content>
+        Content
+      </Content>
+      <Footer>
+        Footer
+      </Footer>
+    </Layout>
   )
 }
-
-export default App

dfbdcc641d8f038f9b999fe3f4b1c190c8dc7b40
tane-git <tane.alexander.wilson@gmail.com>
2022-02-26 10:12:55 +1300

    add some antd components to test variable modifying

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import PrimaryButton from './components/PrimaryButton';
+import { Nav } from './components';
 
 // antd
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
+import 'antd/dist/antd.less';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <div>
+        <Nav />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
